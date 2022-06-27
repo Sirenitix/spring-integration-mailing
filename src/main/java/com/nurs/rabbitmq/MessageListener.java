7e48eda796eade57f9eaeaf0ef050ded4111fcc5
nx <suleevn@gmail.com>
2022-06-27 10:50:58 +0600

    Initial commit

---
+++ src/main/java/com/nurs/rabbitmq/MessageListener.java
@@ -0,0 +1,14 @@
+package com.nurs.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void listener(CustomMessage message) {
+        System.out.println(message);
+    }
+
+}
